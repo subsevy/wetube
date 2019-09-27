ec4800dc0bdcc8312b79410f27accbcd171ae434
subsevy <sub0713@gmail.com>
2019-09-28 01:13:01 +0900

    More Controllers

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,3 +1,4 @@
+import routes from "../routes";
 import { videos } from "../db";
 
 export const home = (req, res) => {
@@ -11,8 +12,17 @@ export const search = (req, res) => {
   res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
-export const upload = (req, res) =>
+export const getUpload = (req, res) => {
   res.render("upload", { pageTitle: "Upload" });
+};
+
+export const postUpload = (req, res) => {
+  const {
+    body: { file, title, description }
+  } = req;
+  // To Do: Upload and save video
+  res.redirect(routes.videoDetail(12344));
+};
 
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
