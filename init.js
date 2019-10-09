a73df84251fe3861acc0ed0c62bb4918f370ce59
subsevy <sub0713@gmail.com>
2019-10-09 21:16:45 +0900

    Making the Header

--- init.js
+++ init.js
@@ -1,9 +1,10 @@
 import "./db";
-import app from "./app";
-import dotenv from "dotenv";
-dotenv.config();
 import "./models/Video";
 import "./models/Comment";
+import dotenv from "dotenv";
+import app from "./app";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
