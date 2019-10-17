2156c7bd3ec2c180b007c4cb0d70352140cf66bf
subsevy <sub0713@gmail.com>
2019-10-18 01:35:18 +0900

    Recording Video 2

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -3,14 +3,31 @@ const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsPreviewVideo");
 
 let streamObject;
+let videoRecorder;
 
-const handleVideoData = event => {};
+const handleVideoData = event => {
+  const { data: videoFile } = event;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click();
+};
+
+const stopRecording = () => {
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerHTML = "Start recording";
+};
 
 const startRecording = () => {
-  const videoRecorder = new MediaRecorder();
+  videoRecorder = new MediaRecorder(streamObject);
   videoRecorder.start();
   videoRecorder.addEventListener("dataavailable", handleVideoData);
+  recordBtn.addEventListener("click", stopRecording);
 };
+
 const getVideo = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
