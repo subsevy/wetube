de4d9166a6d6cfe3e68f000517ac42f536f8b6d7
subsevy <sub0713@gmail.com>
2019-10-18 15:05:28 +0900

    API Registering a view

--- routes.js
+++ routes.js
@@ -31,6 +31,11 @@ const GItHUB_CALLBACK = "/auth/github/callback";
 const FB = "/auth/facebook";
 const FB_CALLBACK = "/auth/facebook/callback";
 
+// API
+
+const API = "/api";
+const REGISTER_VIEW = "/:id/view";
+
 const routes = {
   home: HOME,
   join: JOIN,
@@ -70,7 +75,9 @@ const routes = {
   githubCallback: GItHUB_CALLBACK,
   me: ME,
   facebook: FB,
-  facebookCallback: FB_CALLBACK
+  facebookCallback: FB_CALLBACK,
+  api: API,
+  registerView: REGISTER_VIEW
 };
 
 export default routes;
